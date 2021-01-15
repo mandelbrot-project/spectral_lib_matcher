2f19e5e3ed3f898a39aca29ee9c537e67a9dcf06
PMA <pierre-marie.allard@unige.ch>
2021-01-15 16:45:35 +0100

    adding missing lib and refactoring

--- spectral_lib_matcher.py
+++ spectral_lib_matcher.py
@@ -1,6 +1,7 @@
 # Required global libs
 
 import os
+import sys
 import numpy as np
 import pandas as pd
 
@@ -9,25 +10,54 @@ import pandas as pd
 from matchms.importing import load_from_mgf
 
 
+
+# defining the command line arguments
+try:
+    query_file_path = sys.argv[1]
+    db_file_path = sys.argv[2]
+    parent_mz_tol = sys.argv[3]
+    msms_mz_tol = sys.argv[4]
+    min_cos = sys.argv[5]
+    min_peaks = sys.argv[6]
+    output_file_path = sys.argv[7]
+
+    print('Parsing spectral file '
+          + query_file_path
+          + ' against spectral database: '
+          + db_file_path
+          + '.\n This spectral matching is done with: \n' 
+          + '   - a parent mass tolerance of ' + parent_mz_tol + '\n '
+          + '   - a msms mass tolerance of ' + msms_mz_tol + '\n '
+          + '   - a minimal cosine of ' + min_cos + '\n '
+          + '   - a minimal matching peaks number of ' + min_peaks + '\n '
+          + 'Results will be outputed in ' + ouput_file_path)
+except:
+    print(
+        '''Please add input and output file path as first and second argument, InChI column header as third argument and finally the number of cpus you want to use.
+        Example :
+        python spectral_lib_matcher.py /Users/pma/tmp/Lena_metabo_local/FBMN_metabo_lena/spectra/fbmn_lena_metabo_specs_ms.mgf /Users/pma/tmp/New_DNP_full_pos.mgf 0.01 0.01 0.2 6 /Users/pma/tmp/lena_matched.out''')
+
+
+
 path_data_query = "/Users/pma/tmp/Lena_metabo_local/FBMN_metabo_lena/spectra/"  # enter path to downloaded mgf file
 path_data_db = "/Users/pma/tmp/"  # enter path to downloaded mgf file
 
 file_mgf_query = os.path.join(path_data_query, 
                         "fbmn_lena_metabo_specs_ms.mgf")
 
-spectrums_query = list(load_from_mgf(file_mgf_query))
+spectrums_query = list(load_from_mgf(query_file_path))
 
 file_mgf_db = os.path.join(path_data_db, 
                         "New_DNP_full_pos.mgf")
-spectrums_db = list(load_from_mgf(file_mgf_db))
+spectrums_db = list(load_from_mgf(db_file_path))
 
 
-len(spectrums_query)
-len(spectrums_db)
+# len(spectrums_query)
+# len(spectrums_db)
 
-spectrums_query[0].metadata
+# spectrums_query[0].metadata
 
-spectrums_db[0].metadata
+# spectrums_db[0].metadata
 
 ### some filtering
 
@@ -70,18 +100,21 @@ spectrums_db_cleaned = [peak_processing(s) for s in spectrums_db_cleaned]
 
 
 
-%%time
+#%%time
 from matchms.similarity import PrecursorMzMatch
-similarity_score = PrecursorMzMatch(tolerance=0.01, tolerance_type="Dalton")
+from matchms import calculate_scores
+from matchms.similarity import CosineGreedy
+
+similarity_score = PrecursorMzMatch(tolerance=parent_mz_tol, tolerance_type="Dalton")
 scores = calculate_scores(spectrums_query, spectrums_db_cleaned, similarity_score)
 indices = np.where(np.asarray(scores.scores))
 idx_row, idx_col = indices
-cosine_greedy = CosineGreedy(tolerance=0.01)
+cosine_greedy = CosineGreedy(tolerance=msms_mz_tol)
 data = []
 for (x,y) in zip(idx_row,idx_col):
     if x<y:
         msms_score, n_matches = cosine_greedy.pair(spectrums_query[x], spectrums_db_cleaned[y])[()]
-        if (msms_score>0.2) & (n_matches>1):
+        if (msms_score>min_cos) & (n_matches>min_peaks):
             data.append({'msms_score':msms_score,
                          'matched_peaks':n_matches,
                          'experiment_id':x,
@@ -90,5 +123,5 @@ for (x,y) in zip(idx_row,idx_col):
 df = pd.DataFrame(data)
 
 
-df.to_csv()
+df.to_csv(output_file_path, sep = '\t')
 
