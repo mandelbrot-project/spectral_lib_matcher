a0307baca6d7bc4cf83d00d4b0f13dcc1fe7b2c7
Jonathan Bisson <bjo@uic.edu>
2021-05-26 16:57:34 -0500

    A cleaner approach to all of that (in progress)

---
+++ nostdout.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import contextlib
+import io
+import sys
+
+# define a warning silencer that disables stdout temporarily, use it with a with nostdout(): block
+# see https://stackoverflow.com/a/2829036
+
+@contextlib.contextmanager
+def nostdout(active=True):
+    if active:
+        save_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+    yield
+    if active:
+        sys.stdout = save_stdout
