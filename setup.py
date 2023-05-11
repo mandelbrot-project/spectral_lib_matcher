c6afbc44afee590478ab16aeb10936678339a986
Adriano Rutz <adriano.rutz@hotmail.com>
2023-05-11 08:41:22 +0200

    update

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         'gensim',
         'h5py',
-        'matchms>= 0.18.0',
+        'matchms>= 0.19.0',
         'ms2deepscore >= 0.3.1',
         'numpy',
         'pandas',
